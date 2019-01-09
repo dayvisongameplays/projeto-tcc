c057f013f15892d07a77efa55d76858b76d8cb0b
dayvisongameplays <dayvisoneryc@gmail.com>
2019-01-08 22:25:00 -0300

    Correções na função de justificar faltas.

--- app/controllers/Faltas.java
+++ app/controllers/Faltas.java
@@ -28,7 +28,7 @@ public class Faltas extends Controller {
 
 	public static void listarPorUsuario() throws ParseException {
 		Usuario user = Usuario.findById(Long.parseLong(session.get("idUsuario")));
-		
+
 		List<Atividade> atividades = Atividade.findAll();
 		Date dt2 = null;
 
@@ -46,11 +46,28 @@ public class Faltas extends Controller {
 				if (usuario == user) {
 					Date dt1 = atividade.hrFechamento;
 					if (dt1.compareTo(dt2) < 0) {
-						Falta falta = new Falta();
-						falta.usuario = user;
-						falta.atividade = atividade;
-						falta.dataEnvio = pegarData();
-						falta.save();
+						Falta falta2 = Falta.find("atividade = ?", atividade).first();
+
+						Falta falta3 = new Falta();
+						falta3.dataEnvio = pegarData();
+
+						if (falta2 == null) {
+							Falta falta = new Falta();
+							falta.usuario = user;
+							falta.atividade = atividade;
+							falta.dataEnvio = pegarData();
+							falta.save();
+						} else {
+							if (falta2.dataEnvio.compareTo(falta3.dataEnvio) > 0) {
+								System.out.println(falta3.dataEnvio + " oooo");
+								System.out.println(falta2.dataEnvio + " zzzzz");
+								Falta falta = new Falta();
+								falta.usuario = user;
+								falta.atividade = atividade;
+								falta.dataEnvio = pegarData();
+								falta.save();
+							}
+						}
 					}
 				}
 			}
@@ -81,4 +98,17 @@ public class Faltas extends Controller {
 		return data;
 	}
 
+	public static void justificarFaltas(Long id) {
+		Falta falta = Falta.findById(id);
+		render(falta);
+	}
+
+	public static void Registrar(Long id) throws ParseException {
+		Falta falta = Falta.findById(id);
+		falta.assunto = params.get("falta.assunto");
+		falta.mensagem = params.get("falta.mensagem");
+		falta.enviado = true;
+		falta.save();
+		listarPorUsuario();
+	}
 }
